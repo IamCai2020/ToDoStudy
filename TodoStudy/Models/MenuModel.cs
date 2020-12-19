dd5a304f9fb9580dac756a6b8d34235cf6514a7b
Iamhappy2018 <43491188+Iamhappy2018@users.noreply.github.com>
2020-12-19 15:14:46 +0800

    The first day

---
+++ TodoStudy/Models/MenuModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoStudy.Models
+{
+    class MenuModel
+    {
+        public string IconFont { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ TodoStudy/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using TodoStudy.Models;
+
+namespace TodoStudy.ViewModel
+{
+    public class MainViewModel:ViewModelBase
+    {
+        public MainViewModel()
+        {
+            MenuModels = new ObservableCollection<MenuModel>();
+            menuModels.Add(new MenuModel() { IconFont = "\xe635", Title = "我的一天" });
+            menuModels.Add(new MenuModel() { IconFont = "\xe6b6", Title = "重要" });
+            menuModels.Add(new MenuModel() { IconFont = "\xe6e1", Title = "已计划日程" });
+            menuModels.Add(new MenuModel() { IconFont = "\xe614", Title = "已分配给我" });
+            menuModels.Add(new MenuModel() { IconFont = "\xe755", Title = "任务" });
+        }
+
+/*        private ObservableCollection<MenuModel> menuModels;
+
+        public ObservableCollection<MenuModel> MenuModles
+        {
+            get { return menuModels; }
+            set { menuModels = value;RaisePropertyChanged(); }
+        }*/
+
+        private ObservableCollection<MenuModel> menuModels;
+
+        public ObservableCollection<MenuModel> MenuModels
+        {
+            get { return menuModels; }
+            set { menuModels = value; RaisePropertyChanged(); }
+        }
+
+    }
+
+
+}
